3dfbf0669adf44075832deb90ec27f36bd51dd3f
Thoritie <thor@odds.team>
2025-07-19 07:14:28 +0700

    :zap: init answer db :snowman:

--- init_db.py
+++ init_db.py
@@ -1,20 +1,18 @@
 import sqlite3
 
 def init_database():
-    connection = sqlite3.connect('questions.db')
+    connection = sqlite3.connect('magic8ball.db')
     cursor = connection.cursor()
     print('DB Init')
 
 
     query = """
-        CREATE TABLE IF NOT EXISTS questions (
+        CREATE TABLE IF NOT EXISTS answers (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-	        question TEXT NOT NULL
+	        answer TEXT NOT NULL
         );
     """
     cursor.execute(query)
-
-    # Close the cursor after use
     cursor.close()
 
 if __name__ == '__main__':
