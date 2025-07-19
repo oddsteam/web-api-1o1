eee088253a584267e2c6cdf49d92066c3959e23c
Thoritie <thor@odds.team>
2025-07-19 07:25:06 +0700

    :zap: add todo workshop :snowman:

--- main.py
+++ main.py
@@ -1,18 +1,44 @@
-from fastapi import FastAPI
+import crud
+import random
+from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI(
     title="Let's find the answer",
     description="A fun fortune-telling API that gives you random answers like a Magic 8 Ball."
 )
 
+class Answer(BaseModel):
+    text: str
+
 answer_list = ["YES", "NO", "MAYBE"]
+
 
 @app.get("/8ball")
 def get_answer():
     ## TODO: Random answer
     return { "answer": "Okay inw za"}
 
+
 @app.post("/8ball")
 def add_answer(new_answer: str):
     ## TODO: Update list of answer with new_answer
     return answer_list
+
+
+# TODO Get all answer
+@app.get("/8ball/all")
+def get_all_answer():
+    raise HTTPException(status_code=404, detail="No answers available")
+
+
+# TODO Update answer
+@app.put("/8ball/{id}")
+def update_answer(id: int, answer: Answer):
+    ...
+
+
+# TODO Delete answer
+@app.delete("/8ball/{id}")
+def delete_answer(id: int):
+    ...
